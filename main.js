75bbd118665a89bc57153e055b85fe5fbbec7fc8
Teotonio José de Souza Junior <102707827+teotoniojr@users.noreply.github.com>
2024-03-19 19:55:07 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
 const botoes = document.querySelectorAll('.botao');
+const textos = document.querySelectorAll('.aba-conteudo');34
 
 for(let i= 0; i<botoes.length; i++){
     botoes[i].onclick = function (){
 
         for(j=0; j<botoes.length; j++){
             botoes[j].classList.remove('ativo')
+            textos[j].classList.remove('ativo')
         }
 
         botoes[i].classList.add('ativo')
+        textos[i].classList.add('ativo')
     } 
 }
+
+const contadores = document.querySelectorAll(".contador")
+
+const tempoAtual = new Date(); //Retorna a data atual do computador
+const tempoObjetivo1 = new Date("2024-04-04T00:00:00");
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual
+
